efeac39e511943969bfa8efbbad19fb62b07c075
Amanda de Souza Silva <61766435+a-mandaflores@users.noreply.github.com>
2023-09-29 17:07:34 -0300

    Delete atividade3 directory

--- atividade3/ativ5.py
+++
@@ -1,8 +0,0 @@
-def primo(numero):
-    if numero <= 1:
-        return False
-    for i in range(2, int(numero**0.5) + 1):
-        if numero % i == 0:
-            return False
-    return True
-
--- atividade3/ativ6.py
+++
@@ -1,4 +0,0 @@
-def vogais(string):
-    return sum(1 for i in string if i.lower() in 'aeiou')
-
-print(vogais('amor'))
